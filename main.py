e2a1ac73e5d75735d105742fa3c24027a4013a25
itsrafaelleal <rafaelleal.eduk@gmail.com>
2026-08-18 17:39:57 -0300

    build init transforme das outras tabelas

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #%%
-##comentarios###
-def main():
-    print("Hello from maritimo-data-pipeline!")
+from pathlib import Path
+import pandas as pd
+from datetime import datetime
 
+# Diretório do projeto
+BASE_DIR = Path(__file__).resolve().parent
 
-if __name__ == "__main__":
-    main()
+today = datetime.now().strftime("%Y-%m-%d")
 
-# %%
+# Caminho usando BASE_DIR
+parquet = BASE_DIR / "silver" / "manobras_previstas" / f"{today}_16-39-56.parquet"
+
+df = pd.read_parquet(parquet)
