dd25ce9412aa39a7d3d81878bb9626b6bdbacca7
itsrafaelleal <rafaelleal.eduk@gmail.com>
2026-08-23 17:22:38 -0300

    feat = criado script para camada silver dos fundeados

---
+++ 02_04_trasnform_fundeados.py
@@ -0,0 +1,131 @@
+#%%
+import pandas as pd
+import unicodedata
+from pathlib import Path
+from datetime import datetime
+from io import StringIO
+
+# Diretório do projeto
+BASE_DIR = Path(__file__).resolve().parent
+
+# Diretório onde estão os arquivos de origem
+pasta = BASE_DIR / "landing"
+
+tabela_atual = "navios_fundeados"
+
+# Procura arquivos de atracado realizadas
+arquivos = pasta.glob(
+    f"*{tabela_atual}.html"
+)
+
+# Data de hoje
+today = datetime.now().strftime("%Y-%m-%d")
+
+
+# Encontra o arquivo referente à data de hoje
+arquivo = next(
+    (
+        arquivo
+        for arquivo in arquivos
+        if arquivo.name.startswith(today)
+    ),
+    None
+)
+
+
+# Valida se o arquivo foi encontrado
+if arquivo is None:
+    raise FileNotFoundError(
+        f"Nenhum arquivo encontrado para {today}"
+    )
+
+
+print(f"Arquivo encontrado: {arquivo}")
+
+
+# Lê o HTML como texto
+with open(arquivo, "r", encoding="latin1") as f:
+    html = f.read()
+
+
+# Corrige o problema de encoding
+html = html.encode("latin1").decode("utf-8")
+
+
+# Converte o texto HTML em um objeto semelhante a arquivo
+# para que o pandas possa fazer a leitura
+tabelas = pd.read_html(StringIO(html))
+
+print(f"Tabelas encontradas: {len(tabelas)}")
+
+df = tabelas[0]
+col = df.columns
+print("df colunas ="f'{col}')
+
+#%%       ####    CRIA COLUNAS NOVAS    ####
+
+df["data_processamento"] = datetime.now()
+df["arquivo_origem"] = arquivo.name
+
+
+
+# normalizar e limpar o cabecalho do df
+
+df = df.rename(columns={"Data - Hora": "data_fundeado"})
+df.columns = df.columns.str.lower()
+df.columns = [
+    unicodedata.normalize('NFKD', col)
+    .encode('ascii', 'ignore')
+    .decode('utf-8')
+    for col in df.columns
+]
+print("## NOME DAS COLUNAS ##")
+print(df.columns)
+
+#%%  ### NORMALIMAZAR DADOS ###
+
+colunas = ['navio', 'posicao', 'calado', 'data_fundeado', 'arquivo_origem']
+df[colunas] = df[colunas].apply(
+    lambda col: col.str.replace('[áàãâäÁÀÃÂÄ]', 'a', regex=True)
+                     .str.replace('[éèêëÉÈÊË]', 'e', regex=True)
+                     .str.replace('[íìîïÍÌÎÏ]', 'i', regex=True)
+                     .str.replace('[óòõôöÓÒÕÔÖ]', 'o', regex=True)
+                     .str.replace('[úùûüÚÙÛÜ]', 'u', regex=True)
+                     .str.replace('[çÇ]', 'c', regex=True)
+                     .str.lower()
+)
+
+df["data_processamento"] = datetime.now()
+df["arquivo_origem"] = arquivo.name
+
+print(" ### AMOSTRA DOS DADOS\n")
+
+print(df.head(2))
+
+print("\n ### ")
+
+#%% ### CRIAR SILVER E SALVA 
+
+SILVER_DIR = BASE_DIR / "silver"
+
+pasta_saida = SILVER_DIR / f"{tabela_atual}"
+
+pasta_saida.mkdir(
+    parents=True,
+    exist_ok=True
+)
+
+snapshot = arquivo.stem.removesuffix(
+    f"{tabela_atual}"
+)
+
+arquivo_saida = pasta_saida / f"{snapshot}.parquet"
+
+df.to_parquet(
+    arquivo_saida,
+    engine="pyarrow",
+    index=False
+)
+print(f"Parquet salvo em: {arquivo_saida}")
+
+# %%
